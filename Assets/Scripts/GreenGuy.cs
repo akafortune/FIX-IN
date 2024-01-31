19b024f0be77152d6d8bcb97ac21b8b8b778d130
Connor McDonald <117039387+akafortune@users.noreply.github.com>
2024-01-31 17:33:46 -0500

    Added Stun

--- Assets/Scripts/GreenGuy.cs
+++ Assets/Scripts/GreenGuy.cs
@@ -7,8 +7,8 @@ public class GreenGuy : MonoBehaviour
     PlatformEffector2D[] platforms;
     Rigidbody2D rb;
     public int jumpForce;
-    public float leftAndRight;
-    bool canJump = false;
+    public float leftAndRight, stunClock, stunTime = 3;
+    bool canJump = false, canMove = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,36 +23,50 @@ public class GreenGuy : MonoBehaviour
     {
         float LR = leftAndRight * Time.deltaTime;
         //Debug.Log(rb.velocity.y);
-        if(Input.GetKeyDown(KeyCode.S))
+        if(canMove)
         {
-            foreach (PlatformEffector2D plat in platforms)
+            if (Input.GetKeyDown(KeyCode.S))
             {
-                plat.rotationalOffset = 180;
+                foreach (PlatformEffector2D plat in platforms)
+                {
+                    plat.rotationalOffset = 180;
+                    canJump = false;
+                }
+            }
+            if (Input.GetKeyUp(KeyCode.S))
+            {
+                foreach (PlatformEffector2D plat in platforms)
+                {
+                    plat.rotationalOffset = 0;
+                    canJump = true;
+                }
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(-LR, 0, 0, Space.World);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(LR, 0, 0, Space.World);
+            }
+            if (Input.GetKeyDown(KeyCode.W) && canJump && rb.velocity.y < .25 && !Input.GetKey(KeyCode.S))
+            {
+                //Debug.Log("jump");
+                rb.AddForce(new Vector2(0, jumpForce));
                 canJump = false;
             }
         }
-        if(Input.GetKeyUp(KeyCode.S))
+
+        if (canMove == false)
         {
-            foreach (PlatformEffector2D plat in platforms)
-            {
-                plat.rotationalOffset = 0;
-                canJump = true;
-            }
+            stunClock += Time.deltaTime;
         }
-        if(Input.GetKey(KeyCode.A))
+
+        if (stunClock > stunTime)
         {
-            transform.Translate(-LR, 0, 0, Space.World);
+            canMove = true;
         }
-        if(Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(LR, 0, 0, Space.World);
-        }
-        if (Input.GetKeyDown(KeyCode.W) && canJump && rb.velocity.y < .25 && !Input.GetKey(KeyCode.S))
-        {
-            //Debug.Log("jump");
-            rb.AddForce(new Vector2(0, jumpForce));
-            canJump = false;
-        }
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -60,6 +74,15 @@ public class GreenGuy : MonoBehaviour
         if(collision.gameObject.layer == 7) //platforms
         {
             canJump = true;
+        }
+
+        if(collision.gameObject.layer == 6) //ball
+        {
+            if(canMove)
+            {
+                canMove = false;
+                stunClock = 0;
+            }
         }
     }
 }
