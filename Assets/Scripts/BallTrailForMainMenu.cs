14ed99e277b44f24c7679e9da1d67f8ac71f135f
Dusk-Lynx <Dusklynx44@gmail.com>
2024-02-21 16:03:04 -0500

    Menu Changes

---
+++ Assets/Scripts/BallTrailForMainMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallTrailForMainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.transform.position = new Vector3(-8.5f, 2.42f, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
