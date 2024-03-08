7378224cf474309af13131ad1689c21594088543
Dusk-Lynx <Dusklynx44@gmail.com>
2024-03-08 16:00:44 -0500

    oh shit

---
+++ Assets/Scripts/Gameplay/Ball.cs
@@ -0,0 +1,326 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
+
+public class Ball : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float StaticspeedMultiplier;
+    private float ctdTimer;
+    private bool countingDown;
+    private bool[] countDownAudioPlayed;
+    private TextMeshProUGUI countdownText;
+    public GameObject roundTimer;
+    public float RampspeedMultiplier; //multiplier applies to static
+
+    public float FinalspeedMultiplier;
+    float LastXVelocity;
+    public float augment;
+    private float mapSizeAugment;
+    public GameObject gameOverMenu;
+    private GameObject arrow;
+    public AudioSource audioSource, songSource;
+    public AudioClip wallBounce, paddleBounce, ggBounce, bottomWallBounce, countDownSound, launchSound, ballExplode, defendSong;
+    public TextMeshProUGUI TestVersion;
+
+    private int minAngle, maxAngle, ballAngle;
+
+    public GameObject pauseMenu;
+    public float gameTimer;
+    private Vector3 spawnPos;
+
+    private int bricksBroken;
+
+    private TrailRenderer trail;
+    public static int hits;
+    private GameObject explodingParticle;
+    private Vector3 particleLocation;
+
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        //roundTimer = GameObject.Find("TimerText").GetComponent<TextMeshProUGUI>();
+        roundTimer.SetActive(false);
+        songSource = GameObject.Find("SongSource").GetComponent<AudioSource>();
+        defendSong = (AudioClip) Resources.Load("SFX/UpdatedDefendSong");
+        countDownAudioPlayed = new bool[3];
+        StaticspeedMultiplier = 1.25f;
+        RampspeedMultiplier = 1f;
+        augment = 1.3f;
+        spawnPos = transform.position;
+        particleLocation = transform.position;
+        gameTimer = 0f;
+        rb = GetComponent<Rigidbody2D>();
+        explodingParticle = GameObject.Find("BallPop");
+        explodingParticle.SetActive(false);
+        arrow = GameObject.Find("Pointer");
+        arrow.SetActive(false);
+        trail = GameObject.Find("Trail").GetComponent<TrailRenderer>();
+        countdownText = GameObject.Find("Countdown").GetComponent<TextMeshProUGUI>();
+        countDownSound = (AudioClip) Resources.Load("SFX/Countdown");
+        launchSound = (AudioClip) Resources.Load("SFX/Launch");
+        animator = gameObject.GetComponent<Animator>();
+        //TestVersion = GameObject.Find("TestVersionText").GetComponent<TextMeshProUGUI>();
+        //pauseMenu = GameObject.Find("PauseMenu");
+        if (SceneManager.GetActiveScene() == SceneManager.GetSceneByBuildIndex(3) || SceneManager.GetActiveScene() == SceneManager.GetSceneByBuildIndex(4))
+        {
+            mapSizeAugment = 1.5f;
+            minAngle = 25;
+            maxAngle = 65;
+        }
+        else
+        {
+            mapSizeAugment = 1;
+            minAngle = 15;
+            maxAngle = 45;
+        }
+        ctdTimer = 3.0f;
+        GreenGuy.stunTime /= augment;
+        FinalspeedMultiplier = StaticspeedMultiplier * augment;
+    }
+
+    public void LaunchSequence()
+    {
+        songSource.Pause();
+        ResetBall();
+        Rotate();
+        countingDown = true;
+        ctdTimer = 3.5f;
+    } //Sets the following 4 methods in motion
+
+    public void ResetBall()
+    {
+        countDownAudioPlayed = new bool[3];
+        hits = 0;
+        rb.velocity = Vector3.zero;
+        gameObject.SetActive(true);
+        transform.position = spawnPos;
+        explodingParticle.transform.position = particleLocation;
+        trail.Clear();
+        transform.eulerAngles = new Vector3(0, 0, 180);
+        animator.SetTrigger("Reset");
+    } //Puts the ball back where it started
+
+    public void Rotate()
+    {
+        ballAngle = Random.Range(minAngle, maxAngle + 1); // Randomizes angle of ball
+        //int HorzForce = 0; // Sets angle of ball to 0
+        //Debug.Log(HorzForce);
+        int StartingDirection = Random.Range(0, 2); // 0 for left, 1 for right
+        //Debug.Log(StartingDirection);
+        ballAngle = StartingDirection == 0 ? ballAngle : ballAngle * -1;
+        transform.Rotate(0, 0, ballAngle);
+    } //Sets the ball to random angle
+
+    void Countdown()
+    {
+        roundTimer.SetActive(false);
+        if (ctdTimer > 3)
+        {
+            //waiting a second to play audio
+        }
+        else if (ctdTimer > 2)
+        {
+            if (!countDownAudioPlayed[0])
+            {
+                arrow.SetActive(true);
+                audioSource.PlayOneShot(countDownSound);
+                countDownAudioPlayed[0] = true;
+            }
+            countdownText.text = "3";
+        }
+        else if (ctdTimer > 1)
+        {
+            if (!countDownAudioPlayed[1])
+            {
+                audioSource.PlayOneShot(countDownSound);
+                countDownAudioPlayed[1] = true;
+            }
+            countdownText.text = "2";
+        }
+        else if (ctdTimer > 0)
+        {
+            if (!countDownAudioPlayed[2])
+            {
+                audioSource.PlayOneShot(countDownSound);
+                countDownAudioPlayed[2] = true;
+            }
+            countdownText.text = "1";
+        }
+        else
+        {
+            countdownText.text = "";
+            countingDown = false;
+            Launch();
+        }
+    } //Begins launch countdown and activates indicator arrow
+
+    public void Launch()
+    {
+        roundTimer.SetActive(true); // round timer begins
+        songSource.clip = defendSong;
+        songSource.Play();
+        explodingParticle.transform.position = transform.position;
+        explodingParticle.SetActive(false);
+        BaseBuilding.lastBrickBuilt = true;
+        //transform.Rotate(0, 0, -HorzForce);
+        arrow.SetActive(false);
+        //GreenGuy.speedMod *= augment;
+        //TestVersion.text = "Test Version 1." + augment.ToString("0.00");
+        rb.AddRelativeForce(new Vector2(0, 150 * FinalspeedMultiplier), ForceMode2D.Force);
+        audioSource.PlayOneShot(launchSound);
+        rb.velocity *= 10000f;
+        //Debug.Log(rb.velocity.magnitude);
+    } //Makes the ball start
+
+    void FixedUpdate() //physics fuckery
+    {
+        animator.ResetTrigger("Reset");
+        animator.ResetTrigger("Hit2");
+        animator.ResetTrigger("Hit3");
+        LastXVelocity = rb.velocity.x;
+        //Debug.Log(rb.velocity.x);
+        while (rb.velocity.magnitude > 3.5 * FinalspeedMultiplier) // Prevents ball from going apeshit and flying off the map
+        {
+            rb.velocity = rb.velocity * 0.99f;
+        }
+        //while(rb.velocity.magnitude > 2.0)
+        //{
+        //  rb.velocity = rb.velocity * 1.01f;
+        //}
+
+        //Speed Ramping
+        if (pauseMenu.activeSelf == false)
+        {
+            gameTimer += Time.fixedDeltaTime;
+            RampSpeed();
+        }
+    }
+
+    void Update() // Countdown and Trail color changes
+    {
+        if (countingDown)
+        {
+            ctdTimer -= Time.deltaTime;
+            Countdown();
+        }
+        Color tailColor = trail.startColor;
+        switch (hits)
+        {
+            case 0:
+            case 1:
+                tailColor = new Color(1, 1, 1);
+                break;
+            case 2:
+                tailColor = new Color(255 / 255f, 244 / 255f, 146 / 255f);
+                animator.SetTrigger("Hit2");
+                break;
+            case 3:
+                tailColor = new Color(255 / 255f, 116 / 255f, 37 / 255f);
+                animator.SetTrigger("Hit3");
+                break;
+            //case 4:
+            //    tailColor = new Color();
+            //    break;
+            default:
+                explodingParticle.SetActive(true);
+                audioSource.PlayOneShot(ballExplode);
+                particleLocation = transform.position;
+                LaunchSequence();
+                break;
+        }
+        trail.startColor = tailColor;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) // all of these checks are to keep the ball from bouncing in straight lines
+    {
+        if (collision.gameObject.name.Equals("Wall9PatchRight"))
+        {
+            rb.AddRelativeForce(new Vector2(50, 0), ForceMode2D.Force);
+            int VertForce = rb.velocity.y > 0 ? 50 : -50;
+            rb.AddForce(new Vector2(0, VertForce * FinalspeedMultiplier));
+            audioSource.PlayOneShot(wallBounce);
+            //Debug.Log("R wall hit");
+        }
+        else if (collision.gameObject.name.Equals("Wall9PatchLeft"))
+        {
+            rb.AddRelativeForce(new Vector2(-50, 0), ForceMode2D.Force);
+            int VertForce = rb.velocity.y > 0 ? 50 : -50;
+            rb.AddForce(new Vector2(0, VertForce * FinalspeedMultiplier));
+            audioSource.PlayOneShot(wallBounce);
+            //Debug.Log("L wall hit");
+        }
+        else if (collision.gameObject.name.Equals("Triangle"))
+        {
+            audioSource.PlayOneShot(wallBounce);
+        }
+        else if (collision.gameObject.name.Equals("Paddle"))
+        {
+            hits = 0;
+            animator.SetTrigger("Reset");
+            if (LastXVelocity < 0.5f * mapSizeAugment && LastXVelocity > -0.5f * mapSizeAugment)
+            {
+                //Debug.Log("Fixed X");
+                int HorzForce = LastXVelocity > 0 ? -50 : 50;
+                rb.AddRelativeForce(new Vector2(HorzForce * FinalspeedMultiplier * mapSizeAugment, 0), ForceMode2D.Force);
+            }
+            audioSource.PlayOneShot(paddleBounce);
+        }
+        else if (collision.gameObject.name.Equals("Wall9PatchBottom"))
+        {
+            Time.timeScale = 0f;
+            gameOverMenu.SetActive(true);
+            audioSource.PlayOneShot(bottomWallBounce);
+            songSource.Pause();
+        }
+        else if (collision.gameObject.name.Equals("GreenGuy"))
+        {
+            rb.velocity *= 100;
+            audioSource.PlayOneShot(ggBounce);
+        }
+    }
+
+    void RampSpeed()
+    {
+        if (gameTimer > 30)
+        {
+            float y = gameTimer - 30f;
+            float x = (-1f) * (25f / (Mathf.Pow(y, 2f) + (1.86f * y) + 86.5f)) + (1.3f);
+            if (x < 1)
+            {
+                RampspeedMultiplier = 1;
+            }
+            else
+            {
+                RampspeedMultiplier = x;
+            }
+            FinalspeedMultiplier = (StaticspeedMultiplier *augment) * RampspeedMultiplier;
+            Debug.Log(FinalspeedMultiplier);
+            GreenGuy.stunTime = 1.7f / (augment + (RampspeedMultiplier / 3f));
+        }
+    }
+
+    public void Explode()
+    {
+        hits = 5;
+    }
+
+    public void NewRound(float round)
+    {
+        if (round > 1 && round < 6)
+        {
+            augment+= 0.01f;
+        }
+        gameTimer = 0f;
+        GreenGuy.stunTime = 1.7f / augment;
+        animator.SetTrigger("Reset");
+        FinalspeedMultiplier = StaticspeedMultiplier*augment;
+        Debug.Log(FinalspeedMultiplier);
+    }
+}
---
+++ Assets/Scripts/Gameplay/BaseBuilding.cs
@@ -0,0 +1,245 @@
+using System;
+using System.IO;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+
+public class BaseBuilding : MonoBehaviour
+{
+
+    public static int resources;
+    public TextMeshProUGUI resourcesText;
+    public enum Mode { build, defend };
+    public float roundTime;
+    private float roundCount = 0;
+    public TextMeshProUGUI roundText;
+    public static Mode GameMode;
+    public static bool lastBrickBuilt;
+    private bool firstRound;
+    private float roundClock = 0;
+
+    private AudioSource songSource;
+    private AudioClip buildSong, defendSong;
+    private Animator brickAnimator;
+    public GameObject ball;
+    public GameObject paddle;
+    public GameObject[] Bricks;
+
+    private GameObject BuildUI;
+    private GameObject DefendUI;
+    private TextMeshProUGUI Countdown;
+    private GameObject StartButton;
+    private GameObject RebuildButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        songSource = GameObject.Find("SongSource").GetComponent<AudioSource>();
+        buildSong = (AudioClip) Resources.Load("SFX/BuildMusic");
+        defendSong = (AudioClip) Resources.Load("SFX/UpdatedDefendSong");
+        songSource.clip = defendSong;
+        songSource.Play();
+        songSource.clip = buildSong;
+        songSource.Play();
+        Countdown = GameObject.Find("Countdown").GetComponent<TextMeshProUGUI>();
+        firstRound = true;
+        Bricks = getBrickArray();
+        resources = 45;
+        GameMode = Mode.build;
+        ball = GameObject.Find("Ball");
+        ball.SetActive(false);
+        paddle = GameObject.Find("Paddle");
+        paddle.SetActive(false);
+        BuildUI = GameObject.Find("BuildUI");
+        DefendUI = GameObject.Find("DefendUI");
+        RebuildButton = GameObject.Find("Rebuild Button");
+        DefendUI.SetActive(false);
+        lastBrickBuilt = false;
+        GreenGuy.buildTimer = 0.65f;
+        roundTime = 63.5f;
+        roundCount++;
+
+        if (!Directory.Exists(Application.persistentDataPath + "/SaveData"))
+        {
+            Directory.CreateDirectory(Application.persistentDataPath + "/SaveData");
+        }
+        if (!File.Exists(Application.persistentDataPath + "/SaveData/lastRound1.txt"))
+        {
+            StreamWriter sw = new StreamWriter(Application.persistentDataPath + "/SaveData/lastRound1.txt");
+            sw.WriteLine(resources);
+            sw.Close();
+        }
+
+        StreamReader sr = new StreamReader(Application.persistentDataPath + "/SaveData/lastRound1.txt");
+        int savedResources = Convert.ToInt32(sr.ReadLine());
+        sr.Close();
+        if (resources == savedResources)
+        {
+            RebuildButton.SetActive(false);
+        }
+        roundText = GameObject.Find("RoundNumberText").GetComponent<TextMeshProUGUI>();
+    }
+
+    private GameObject[] getBrickArray()
+    {
+        Transform[] array1 = GameObject.Find("Layer 1").GetComponentsInChildren<Transform>();
+        Transform[] array2 = GameObject.Find("Layer 2").GetComponentsInChildren<Transform>();
+        Transform[] array3 = GameObject.Find("Layer 3").GetComponentsInChildren<Transform>();
+        Transform[] array4 = GameObject.Find("Layer 4").GetComponentsInChildren<Transform>();
+        Transform[] array5 = GameObject.Find("Layer 5").GetComponentsInChildren<Transform>();
+
+        Transform[] combinedArray;
+
+        combinedArray = array1.Concat(array2).Concat(array3).Concat(array4).Concat(array5).ToArray();
+        int index = 0;
+        GameObject[] finalArray = new GameObject[50];
+        foreach (Transform t in combinedArray)
+        {
+            if (t.gameObject.name.Equals("Brick(Clone)"))
+            {
+                finalArray[index] = t.gameObject;
+                index++;
+            }
+        }
+
+        return finalArray;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        resourcesText.text = Convert.ToString(resources);
+
+        if(GameMode == Mode.defend)
+        {
+            roundClock += Time.deltaTime;
+
+            if(roundClock >= roundTime)
+            {
+                BeginBuild();
+                roundClock = 0;
+            }
+        }
+        roundText.text = roundCount.ToString();
+    }
+
+    /*
+    void CountdownToStart()
+    {
+        if (ctdTimer > 2)
+        {
+            countdown.text = "3";
+        }
+        else if (ctdTimer > 1)
+        {
+            countdown.text = "2";
+        }
+        else if (ctdTimer > 0)
+        {
+            countdown.text = "1";
+        }
+        else
+        {
+            countdown.text = "";
+            ctd =false;
+            StartDefend();
+        }
+    }*/
+
+    public static void Spend(int value)
+    {
+        resources -= value;
+    }
+
+    public void BeginRound()
+    {
+        if(firstRound)
+        {
+            StreamWriter sw = new StreamWriter(Application.persistentDataPath + "/SaveData/lastRound1.txt");
+            sw.WriteLine(resources);
+            foreach(GameObject brick in Bricks)
+            {
+                sw.Write(brick.GetComponent<Brick>().isBuilt() + ",");
+            }
+            sw.Close();
+            firstRound = false;
+            RebuildButton.SetActive(false);
+        }
+        foreach (GameObject brick in Bricks)
+        {
+            //brick.GetComponent<Animator>().SetFloat("FixMultiplier",.65f);
+            if (brick.GetComponent<Collider2D>().isTrigger)
+            {
+                brick.SetActive(false);
+            }
+        }
+        GameMode = Mode.defend;
+        DefendUI.SetActive(true);
+        BuildUI.SetActive(false);
+        paddle.SetActive(true);
+        ball.SetActive(true);
+        ball.GetComponent<Ball>().LaunchSequence();
+        
+        ball.GetComponent<Ball>().NewRound(roundCount);
+    }
+
+    public void BeginBuild()
+    {
+        Countdown.text = "";
+        foreach(GameObject brick in Bricks)
+        {
+            if (!brick.activeInHierarchy)
+            {
+                brick.SetActive(true);
+                Animator brickAnim = brick.GetComponent<Animator>();
+                brickAnim.SetBool("IsBroken", true);
+                brickAnim.Play("BrokenBrick", 0, 0);
+            }
+        }
+        songSource.clip = buildSong;
+        songSource.UnPause();
+        resources += 15;
+        GameMode = Mode.build;
+        DefendUI.SetActive(false);
+        BuildUI.SetActive(true);
+        paddle.SetActive(false);
+        ball.SetActive(false);
+        roundCount++;
+    }
+
+    public void SkipBuild()
+    {
+        Brick[] Layer1 = GameObject.Find("Layer 1").GetComponentsInChildren<Brick>();
+        Brick[] Layer2 = GameObject.Find("Layer 2").GetComponentsInChildren<Brick>();
+
+        BuildLayer(Layer1);
+        BuildLayer(Layer2);
+        BeginRound();
+    }
+
+    public void BuildLayer(Brick[] array)
+    {
+        foreach(Brick brick in array)
+        {
+            brick.fixBrick();
+        }
+    }
+
+    public void BuildLast()
+    {
+        StreamReader sr = new StreamReader(Application.persistentDataPath + "/SaveData/lastRound1.txt");
+        resources = Convert.ToInt32(sr.ReadLine());
+        string[] brickArrangement = sr.ReadLine().Split(',');
+        int index = 0;
+        foreach(GameObject brick in Bricks)
+        {
+            if (Convert.ToBoolean(brickArrangement[index]))
+                brick.GetComponent<Brick>().fixBrick();
+            else
+                brick.GetComponent<Brick>().cancelBrick();
+            index++;
+        }
+        sr.Close();
+        RebuildButton.SetActive(false);
+    }
+}
