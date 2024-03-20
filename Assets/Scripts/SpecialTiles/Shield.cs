ee3c1d60877bc191f098a743f4da09dffa0ea77f
Dusk-Lynx <Dusklynx44@gmail.com>
2024-03-20 13:33:59 -0400

    Shield Particles

--- Assets/Scripts/SpecialTiles/Shield.cs
+++ Assets/Scripts/SpecialTiles/Shield.cs
@@ -7,6 +7,8 @@ public class Shield : SpecialTile
     float cooldownStart;
     float cooldownLength;
     GameObject shield;
+
+    public Animator animator;
     // Start is called before the first frame update
     protected override void Start()
     {
@@ -15,6 +17,9 @@ public class Shield : SpecialTile
         effectLength = 10;
         cooldownLength = 10;
         cooldownStart = -10;
+
+        animator = GameObject.Find("Sheild Particles").transform.GetChild(0).gameObject.GetComponent<Animator>();
+        animator.SetBool("Stop", true);
     }
     protected override void OnTriggerStay2D(Collider2D collision)
     {
@@ -26,11 +31,19 @@ public class Shield : SpecialTile
     protected override void doAction()
     {
         shield.SetActive(true);
+        animator.SetBool("Stop", false);
+        animator.SetTrigger("Start");
     }
 
     protected override void stopAction()
     {
         shield.SetActive(false);
         cooldownStart = Time.time;
+        animator.SetBool("Stop", true);
+    }
+
+    void FixedUpdate()
+    {
+        animator.ResetTrigger("Start");
     }
 }
