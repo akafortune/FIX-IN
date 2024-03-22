0220913f942764070e4beb21cc8b6091d7f25e57
Dusk-Lynx <Dusklynx44@gmail.com>
2024-03-22 14:23:42 -0400

    Added Bubble Prefab

---
+++ Assets/Visual/Partcles stuff/BubbleUpDown.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleUpDown : MonoBehaviour
+{
+    public float timer = 5f;
+    public bool up = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer > 0)
+        {
+            timer -=Time.deltaTime;
+            if (up)
+            {
+                gameObject.transform.position += new Vector3(0, 0.0005f, 0);
+            }
+            if (!up)
+            {
+                gameObject.transform.position -= new Vector3(0, 0.0005f, 0);
+            }
+        }
+        else
+        {
+            timer = 5f;
+            up = !up;
+        }
+    }
+}
