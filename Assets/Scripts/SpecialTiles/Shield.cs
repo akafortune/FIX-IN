81c0f69df2635d0f61df9794fa036315ba7ace57
Ben Scholl <xpclippy@gmail.com>
2024-03-23 02:20:20 -0400

    fixed weird floor stuff and added shield size change

--- Assets/Scripts/SpecialTiles/Shield.cs
+++ Assets/Scripts/SpecialTiles/Shield.cs
@@ -7,6 +7,8 @@ public class Shield : SpecialTile
     float cooldownStart;
     float cooldownLength;
     GameObject shield;
+    GameObject shieldParticles;
+    int hits;
 
     public Animator animator;
     // Start is called before the first frame update
@@ -14,7 +16,8 @@ public class Shield : SpecialTile
     {
         base.Start();
         shield = transform.GetChild(0).gameObject;
-        effectLength = 10;
+        shieldParticles = transform.GetChild(1).transform.GetChild(0).gameObject;
+        effectLength = 100;
         cooldownLength = 10;
         cooldownStart = -10;
 
@@ -23,16 +26,34 @@ public class Shield : SpecialTile
     }
     protected override void OnTriggerStay2D(Collider2D collision)
     {
-        if(cooldownStart + cooldownLength < Time.time)
+        if(cooldownStart + cooldownLength < Time.time && BaseBuilding.GameMode != BaseBuilding.Mode.build)
         {
             base.OnTriggerStay2D(collision);
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name.Equals("Ball"))
+        {
+            hits++;
+            if(hits == 4)
+            {
+                stopAction();
+                effectActive = false;
+            }
+            IEnumerator Routine = decreaseSizeRoutine(1-(hits*.1f));
+            StartCoroutine(Routine);
+        }
+    }
     protected override void doAction()
     {
+        shield.transform.localScale = (Vector3.one);
+        shieldParticles.transform.localScale = (Vector3.one);
         shield.SetActive(true);
         animator.SetBool("Stop", false);
         animator.SetTrigger("Start");
+        hits = 0;
     }
 
     protected override void stopAction()
@@ -45,5 +66,18 @@ public class Shield : SpecialTile
     void FixedUpdate()
     {
         animator.ResetTrigger("Start");
+    }
+
+    IEnumerator decreaseSizeRoutine(float size)
+    {
+        while (shield.transform.localScale.x > size)
+        {
+            Debug.Log("Doing the thing");
+            shield.transform.localScale = (shield.transform.localScale *= .995f);
+            shieldParticles.transform.localScale = (shieldParticles.transform.localScale *= .995f);
+            yield return new WaitForFixedUpdate();
+        }
+        yield return null;
+
     }
 }
