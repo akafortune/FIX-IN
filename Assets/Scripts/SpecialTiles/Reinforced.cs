af0b76286c0dcadfc0500585bb3a7981b1c07d11
Connor McDonald <117039387+akafortune@users.noreply.github.com>
2024-04-05 19:51:05 -0400

    reinforced animations

--- Assets/Scripts/SpecialTiles/Reinforced.cs
+++ Assets/Scripts/SpecialTiles/Reinforced.cs
@@ -5,18 +5,22 @@ using UnityEngine;
 public class Reinforced : SpecialTile
 {
     int hits;
+    public Animator animator;
     // Start is called before the first frame update
     protected override void Start()
     {
         hits = 3;
+        animator.SetInteger("HP", 3);
+        animator.SetBool("IsBroken", false);
         base.Start();
     }
 
     // Update is called once per frame
     protected override void cancelBrick()
     {
-        base.cancelBrick();
-        Destroy(this.gameObject);
+        animator.SetBool("IsBroken", true);
+        associatedBrick.removeSpecialBrick();
+
     }
 
     protected override void OnCollisionEnter2D(Collision2D collision)
@@ -26,10 +30,12 @@ public class Reinforced : SpecialTile
         if (ballHit && Brick.canBreak)
         {
             hits--;
+            animator.SetInteger("HP", hits);
             if (hits <= 0)
             {
+                animator.SetBool("IsBroken", true);
                 base.OnCollisionEnter2D(collision);
-                Destroy(this.gameObject);
+                associatedBrick.removeSpecialBrick();
             }
             Brick.canBreak = false;
         }
