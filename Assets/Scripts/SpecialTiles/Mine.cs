284c641b7ca29a6489a65e110ad7d58dd6be1693
Adam Palmer <Dusklynx44@gmail.com>
2024-04-05 23:07:50 -0400

    fun with particles

--- Assets/Scripts/SpecialTiles/Mine.cs
+++ Assets/Scripts/SpecialTiles/Mine.cs
@@ -7,16 +7,19 @@ public class Mine : SpecialTile
 {
     bool canMine;
     SpriteRenderer mineIcon;
+    ParticleSystem ps;
     public new void Start()
     {
         base.Start();
         canMine = true;
         mineIcon = transform.GetChild(0).GetComponent<SpriteRenderer>();
+        ps = gameObject.transform.GetChild(1).gameObject.GetComponent<ParticleSystem>();
     }
     public new void doAction()
     {
         BaseBuilding.resources += 1;
         canMine = false;
+        ps.Play();
         IEnumerator coroutine = waitToMine();
         StartCoroutine(coroutine);
     }
@@ -38,7 +41,7 @@ public class Mine : SpecialTile
 
     protected IEnumerator waitToMine()
     {
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(1.5f);
         canMine = true;
     }
 
