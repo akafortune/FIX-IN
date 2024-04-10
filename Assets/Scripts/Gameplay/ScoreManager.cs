6f71fad2be2683134bca93cb18ffe56e7fd20fd3
Ben Scholl <xpclippy@gmail.com>
2024-04-09 23:53:21 -0400

    high scores pt1
    not done with file i/o or loading in yet

--- Assets/Scripts/Gameplay/ScoreManager.cs
+++ Assets/Scripts/Gameplay/ScoreManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -12,11 +13,29 @@ public class ScoreManager : MonoBehaviour
     public int scoreGrowthRate = 1;
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI highScoreText;
+    public SpecialTile[] sTiles;
+    KeyValuePair<string, int>[] topScores;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        string scoreDataPath = Application.persistentDataPath + "/SaveData/Scores";
+        if (!File.Exists(scoreDataPath))
+        {
+            File.Create(scoreDataPath);
+            StreamWriter scoreWriter = new StreamWriter(scoreDataPath);
+            scoreWriter.WriteLine("GRN GUY,000");
+            scoreWriter.WriteLine("FRK GUY,000");
+            scoreWriter.WriteLine("DEV GUY,000");
+            scoreWriter.Close();
+        }
+
+        topScores = new KeyValuePair<string, int>[3];
+        StreamReader scoreReader = new StreamReader(scoreDataPath);
+        string[] inputs = new string[3];
+        inputs[0] = scoreReader.ReadLine();
+        inputs[1] = scoreReader.ReadLine();
+        inputs[2] = scoreReader.ReadLine();
     }
 
     // Update is called once per frame
